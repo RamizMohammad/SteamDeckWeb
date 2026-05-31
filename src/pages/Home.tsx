fc205304f05246259091e88131fb5c8092429646
Abhisheek <abhisheekmondal927@gmail.com>
2026-06-01 00:38:34 +0530

    final update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -737,7 +737,7 @@ const Home: React.FC = () => {
                 <Github className="w-4 h-4 sm:w-5 sm:h-5 text-white group-hover:text-[#00B4FF] transition-colors" />
               </a>
               <a
-                href="https://twitter.com"
+                href="https://X.com"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-9 h-9 sm:w-10 sm:h-10 rounded-lg border border-white/10 flex items-center justify-center hover:border-[#00B4FF] hover:bg-[#00B4FF]/10 transition-all group"
